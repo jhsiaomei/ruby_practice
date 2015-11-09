35728bdcc93f10aa545b21637c549ac9ec84e420
jhsiaomei <jw1314@gmail.com>
2015-11-08 19:19:01 -0600

    solved anagrams

--- anagrams.rb
+++ anagrams.rb
@@ -1,7 +1,16 @@
 # Determine if two strings are anagrams (case insensitive)
-
+def anagrams?(word_original, word_new)
+  if word_original.downcase.split(//).sort == word_new.downcase.split(//).sort
+    return true
+  else
+    return false
+  end
+end
 
 # Driver code
 p anagrams?("cinema", "iceman") # should be true
 p anagrams?("Cinema", "IceMan") # should be true
 p anagrams?("baseball", "ballbass") # should be false
+
+
+
