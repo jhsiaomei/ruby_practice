2114b830dd7007d83586742c6e23cb41eafb2808
jhsiaomei <jw1314@gmail.com>
2015-11-08 21:33:38 -0600

    reverse sentence

---
+++ reverse_sentence.rb
@@ -0,0 +1,10 @@
+# Reverse a sentence string word by word. 
+# For example, given s = "the sky is blue", return "blue is sky the".
+def reverse_sentence(string)
+  string.split.reverse.join(" ")
+end
+
+# Driver code
+p reverse_sentence("the sky is blue") # should be "blue is sky the"
+p reverse_sentence("What a wonderful kind of day!") # should be "day! of kind wonderful a What"
+p reverse_sentence("hello") # should be "hello"
